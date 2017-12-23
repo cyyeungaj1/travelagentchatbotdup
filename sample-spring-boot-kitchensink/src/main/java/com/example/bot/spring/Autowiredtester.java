4838b30f63e2220e2f59d63f17951fcdc3aa9496
cyyeungaj <cyyeungaj@connect.ust.hk>
2017-12-23 21:22:30 +0800

    add Autowired tester files

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Autowiredtester.java
@@ -0,0 +1,10 @@
+package com.example.bot.spring;
+// import lombok.extern.slf4j.Slf4j;
+// import org.springframework.stereotype.Component;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.beans.factory.annotation.Qualifier;
+
+
+public interface Autowiredtester{
+  public String test();
+}
